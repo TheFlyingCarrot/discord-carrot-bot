172c44ac4c9cfcec3ec733d99a094207f0cac4a4
Creedon H <CreedonH@users.noreply.github.com>
2019-12-27 01:43:15 -0700

    read/write changes

--- extra/setGuildData.js
+++ extra/setGuildData.js
@@ -1,13 +1,10 @@
 const fs = require('fs')
 module.exports = {
 	write(guild, newData) {
-        console.log('___setGuildData.js')
 		if ((newData) && (fs.existsSync(`./guilds/${guild.id}.json`))) {
-            console.log(`Guild ID File Found for Guild ID: ${guild.id}`)
+            // eslint-disable-next-line no-unused-vars
             const guildFile = JSON.parse(fs.readFileSync(`./guilds/${guild.id}.json`))
             try {
-                console.log(`DISCORD\nName: ${guild.name}\nID: ${guild.id}`)
-                console.log(`FILESYSTEM\nName: ${guildFile.name}\nID: ${guildFile.id}\nAdmin Role ID: ${guildFile.adminRoleID}\nMod Role ID: ${guildFile.modRoleID}`)
                 fs.writeFileSync(`./guilds/${guild.id}.json`, JSON.stringify(newData), 'utf-8')
             } catch (err) {
                 console.log(err)
@@ -20,7 +17,6 @@ module.exports = {
                 modRoleID: NaN,
             }
             try {
-                console.log(`DISCORD\nName: ${guild.name}\nID: ${guild.id}`)
                 fs.writeFileSync(`./guilds/${guild.id}.json`, JSON.stringify(defaultData), 'utf-8')
             } catch (err) {
                 console.log(err)
