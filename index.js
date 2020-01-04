a9abebbade84307f20043d50078d20b3981f3970
Creedon H <CreedonH@users.noreply.github.com>
2020-01-04 02:21:28 -0700

    logging fix

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ client.on('message', message => {
 	// START Execute Command
 	try {
 		command.execute(message, args)
-		console.log(`[${message.author.id}] [${message.author.name}] used command [${commandName}] with args [${args}].`)
+		console.log(`[${message.author.id}] [${message.author}] used command [${commandName}] with args [${args}].`)
 	} catch (err) {
 		console.log(err)
 		message.reply('there was an error trying to execute that command.')
