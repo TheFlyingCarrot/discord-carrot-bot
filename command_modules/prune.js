bb0aaf3a0f388f25f6177f98e036125e0d774021
Creedon H <CreedonH@users.noreply.github.com>
2020-01-06 08:17:42 -0700

    reformat

--- command_modules/prune.js
+++ command_modules/prune.js
@@ -8,28 +8,28 @@ module.exports = {
 	requiredRole: ['admin', 'mod'],
 	execute(message, args) {
 		if (!message.member.hasPermission(`${this.permission}`, false, true, true)) {
-			message.reply('you do not have permission to use this command.')
+			message.channel.send(`${message.author}you do not have permission to use this command.`)
 			return null
 		}
 		const amount = parseInt(args[0])
 		if (isNaN(amount)) {
 			message.channel.bulkDelete(2, true).catch(err => {
 				console.error(err)
-				message.reply(`There was an error executing that command. Error: ${err}`)
+				message.channel.send(`${message.author}There was an error executing that command. Error: ${err}`)
 				return null
 			})
 		} else if ((amount < 2 || amount > 100)) {
 			if (Number.isInteger(amount)) {
-				message.reply('You must input a number between 2 and 99.')
+				message.channel.send(`${message.author}You must input a number between 2 and 99.`)
 				return null
 			} else {
-				message.reply('You must input a number between 2 and 99 that is also an integer.')
+				message.channel.send(`${message.author}You must input a number between 2 and 99 that is also an integer.`)
 				return null
 			}
 		} else {
 			message.channel.bulkDelete(amount, true).catch((error) => {
 				console.error(error)
-				message.reply(`There was an error executing that command. ${error}`)
+				message.channel.send(`${message.author}There was an error executing that command. ${error}`)
 				return null
 			})
 		}
