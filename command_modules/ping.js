8e1f86f1cb5f35c3a55aace036276429be84b86b
TheFlyingCarrot <32348022+TheFlyingCarrot@users.noreply.github.com>
2020-01-08 16:25:13 -0700

    Update ping.js

--- command_modules/ping.js
+++ command_modules/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	execute(message) {
+	execute(itemTable) {
+                const { client, message, args, templateEmbed } = itemTable
 		message.channel.send('Pong.')
 	},
 }
