ccb87874a29e397b8ffcb1ebfa3fad75799d5d46
Creedon H <CreedonH@users.noreply.github.com>
2020-01-12 20:22:50 -0700

    qrs

---
+++ command_modules/qrcode.js
@@ -0,0 +1,41 @@
+const QRCode = require('qrcode')
+if (!Array.prototype.last) {
+	Array.prototype.last = function() {
+		return this[this.length - 1]
+	}
+}
+module.exports = {
+	name: 'qrcode',
+	aliases: ['qr', 'q'],
+	usage: '[link]',
+	description: 'Generate a QR code.',
+	cooldown: 30,
+	execute(itemTable) {
+		const { client, message, args, templateEmbed } = itemTable
+		if (!args) {
+			return null
+		} else {
+			let argStitch = ''
+			args.forEach(arg => {
+				argStitch += ((args.last() === arg) ? (`${arg}`) : (`${arg} `))
+				console.log(argStitch)
+			})
+			QRCode.toFile(`./qrcodes/${message.author.id}.png`, String(argStitch), {
+				color: {
+					dark: '#F00',
+					light: '#0000',
+				},
+			}, function(err) {
+				if (err) throw err
+				const newEmbed = templateEmbed
+					.setAuthor('Carrot Bot', 'https://i.ibb.co/v3d9t9x/carrot-clip-art.png')
+					.setThumbnail('https://i.ibb.co/VTS5PXk/user-write.png')
+					.setTimestamp()
+					.setTitle('QR Code')
+					.addField('**Conents**', argStitch)
+					.attachFile(`./qrcodes/${message.author.id}.png`)
+				message.channel.send(newEmbed)
+			})
+		}
+	},
+}
