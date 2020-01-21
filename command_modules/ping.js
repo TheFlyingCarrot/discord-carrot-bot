d681899f985704d41c40ecf01ee5ade5bb22f89d
Creedon H <CreedonH@users.noreply.github.com>
2020-01-21 01:24:51 -0700

    Prune fix.

--- command_modules/ping.js
+++ command_modules/ping.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(itemTable) {
-                const { client, message, args, templateEmbed } = itemTable
+		const { client, message, args, templateEmbed } = itemTable
 		message.channel.send('Pong.')
 	},
 }
--- command_modules/prune.js
+++ command_modules/prune.js
@@ -6,30 +6,31 @@ module.exports = {
 	guildOnly: true,
 	permission: 'MANAGE_MESSAGES',
 	requiredRole: ['admin', 'mod'],
-	execute(message, args) {
+	execute(itemTable) {
+		const { client, message, args, templateEmbed } = itemTable
 		if (!message.member.hasPermission(`${this.permission}`, false, true, true)) {
-			message.channel.send(`${message.author}you do not have permission to use this command.`)
+			message.channel.send(`${message.author}, you do not have permission to use this command.`)
 			return null
 		}
 		const amount = parseInt(args[0])
 		if (isNaN(amount)) {
 			message.channel.bulkDelete(2, true).catch(err => {
 				console.error(err)
-				message.channel.send(`${message.author}There was an error executing that command. Error: ${err}`)
+				message.channel.send(`${message.author}, there was an error executing that command. Error: ${err}`)
 				return null
 			})
 		} else if ((amount < 2 || amount > 100)) {
 			if (Number.isInteger(amount)) {
-				message.channel.send(`${message.author}You must input a number between 2 and 99.`)
+				message.channel.send(`${message.author}, you must input a number between 2 and 99.`)
 				return null
 			} else {
-				message.channel.send(`${message.author}You must input a number between 2 and 99 that is also an integer.`)
+				message.channel.send(`${message.author}, you must input a number between 2 and 99 that is also an integer.`)
 				return null
 			}
 		} else {
 			message.channel.bulkDelete(amount, true).catch((error) => {
 				console.error(error)
-				message.channel.send(`${message.author}There was an error executing that command. ${error}`)
+				message.channel.send(`${message.author}, there was an error executing that command. ${error}`)
 				return null
 			})
 		}
