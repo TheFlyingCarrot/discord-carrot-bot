ed779f21485f93ce457ab24a513d8e3e114ece5e
Creedon Hood <creedonhood@gmail.com>
2020-06-03 17:53:29 -0600

    play dep

--- command_modules/play.js
+++ command_modules/play.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 module.exports = {
 	enabled: false,
-	can_toggle: true,
+	can_toggle: false,
 	name: 'play',
 	usage: '[link]',
 	description: 'Play a tune.',
@@ -14,15 +14,7 @@ module.exports = {
 			if (!args) {
 				message.channel.send(`${message.author}, you didn't give any song to play.`)
 			} else {
-				const connection = await message.member.voice.channel.join()
-				// add song to queue
-				// message.channel.send(`${message.author}, ${newSong} was added to the queue.`)
-				if (args[0] == 'juicy') {
-					const dispatcher = connection.play(fs.createReadStream('./dump/audio/juicy.ogg'), { type: 'ogg/opus', highWaterMark: 15 })
-					dispatcher.on('finish', () => {
-						dispatcher.destroy()
-					})
-				}
+				// TODO: integrate actually playing audio
 			}
 		} else {
 			message.channel.send(`${message.author}, you need to join a voice channel first!`)
