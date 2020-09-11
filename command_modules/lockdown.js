5f2669385689d8f9e9d73258064979ad21a67f4d
Creedon Hood <creedonhood@gmail.com>
2020-09-11 09:10:21 -0600

    Silent Lockdown

---
+++ command_modules/lockdown.js
@@ -0,0 +1,42 @@
+const Validate_Flag = Flag => Flag === 'true' || Flag === 'false' || Flag === 'null'
+
+const Ignored_Channels = new Set([
+	750499239171457064, 750505158043107509, 750498579797377196,
+	442002580764360715, 442004978111086592, 731313604493574145, 731299966341087273,
+])
+
+module.exports = {
+	enabled: true,
+	canToggle: true,
+	name: 'lockdown',
+	aliases: ['lock-down'],
+	usage: '[true/false/null]',
+	args: true,
+	description: 'Lockdown the server.',
+	cooldown: 30,
+	guildOnly: true,
+	permission: 'ADMINISTRATOR',
+	execute(dataTable) {
+		// eslint-disable-next-line no-unused-vars
+		const { client, message, args, templateEmbed } = dataTable
+		if (!message.member.hasPermission(`${this.permission}`, false, true, true)) {
+			message.channel.send(`${message.author}, you do not have permission to use that command.`)
+			return null
+		}
+		if (!Validate_Flag(args[0])) {
+			message.channel.send(`${message.author}, you did not provide a valid flag.`)
+			return null
+		}
+		const flag = flag.toLowerCase() === 'true' ? true : (flag.toLowerCase() === 'false' ? false : null)
+		const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category')
+		channels.forEach(channel => {
+			if (!Ignored_Channels.has(channel.id)) {
+				channel.updateOverwrite(message.guild.roles.everyone.id, {
+					SEND_MESSAGES: !flag,
+				}, `Lockdown by: ${message.author.tag}`).catch(err => console.log(err))
+			} else {
+				console.log(`Skipping channel: [${channel.name}]:[${channel.id}]`)
+			}
+		})
+	},
+}
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const debug_logger = require('./helper_modules/debug_logger')
 const reaction_handler = require('./helper_modules/reaction_handler')
 const cooldown_handler = require('./helper_modules/cooldown_handler')
 const command_validator = require('./helper_modules/command_validator')
-const perf = require('execution-time')(console.log)
 
 // Discord Initialization
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
@@ -27,7 +26,6 @@ client.once('ready', () => {
 
 // Triggers when any message in any guild has a reaction added to it.
 client.on('messageReactionAdd', async (reaction, user) => {
-	perf.start('Reaction Processing')
 	if (reaction.partial) {
 		try {
 			await reaction.fetch()
@@ -38,22 +36,17 @@ client.on('messageReactionAdd', async (reaction, user) => {
 	}
 
 	reaction_handler.execute({ client, reaction, user })
-
-	perf.stop('Message Processing')
 })
 
 // Triggers when any new message is recieved by the bot (client).
 client.on('message', message => {
-	perf.start('Message Processing')
 	// Command Validator | ./helper_modules/command_validator
 	const { command, args } = command_validator.execute({ client, message, prefix, developers, cooldowns, debug_logger, Discord })
 	if (!command || !args) {
-		perf.stop('Message Processing')
 		return null
 	}
 	// Cooldown Handling | ./helper_modules/cooldown_handler
 	if (!cooldown_handler.execute({ message, command, cooldowns, developers })) {
-		perf.stop('Message Processing')
 		return null
 	}
 	// Command Execution
@@ -63,8 +56,6 @@ client.on('message', message => {
 	} catch (err) {
 		console.log(err)
 		message.channel.send(`${message.author}, \`${command.name}\` produced an unknown error.`)
-	} finally {
-		perf.stop('Message Processing')
 	}
 })
 
