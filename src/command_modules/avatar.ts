ce640e03ae76fd9e25b2ba05ad4bb446e9a68e36
Creedon Hood <creedonhood@gmail.com>
2020-10-04 06:12:46 -0600

    Avatar fix

--- src/command_modules/avatar.ts
+++ src/command_modules/avatar.ts
@@ -1,5 +1,5 @@
 import { Command, ExtendedClient } from '../typings.js'
-import Discord, { Client, Message, MessageEmbed } from '../internal.js'
+import Discord, { User, Client, Message, MessageEmbed } from '../internal.js'
 
 const avatar: Command = {
     name: 'avatar',
@@ -11,21 +11,14 @@ const avatar: Command = {
 
     execute ({ client, message, args }: { client: Client, message: Message, args: string[] }, Debugging: boolean): string | null | void {
         const newEmbed = new MessageEmbed()
-        if (!message.mentions.users.size) {
-            newEmbed.setAuthor('Carrot Bot', 'https://i.ibb.co/v3d9t9x/carrot-clip-art.png')
-                .setThumbnail('https://i.ibb.co/xXQbnn5/user-menu.png')
-                .setTimestamp()
-                .setTitle('Avatar Command')
-                .addField('**Your Avatar**', `<${message.author.displayAvatarURL}>`)
-        } else {
-            newEmbed.setAuthor('Carrot Bot', 'https://i.ibb.co/v3d9t9x/carrot-clip-art.png')
-                .setThumbnail('https://i.ibb.co/xXQbnn5/user-menu.png')
-                .setTimestamp()
-                .setTitle('Avatar Command')
-            message.mentions.users.map(user => {
-                newEmbed.addField(`**${user.tag}'s Avatar**`, `<${user.displayAvatarURL}>`)
-            })
-        }
+        const user = message.mentions.users.map(userObject => userObject).shift()
+        console.log(user)
+        const userAvatar = message.mentions.users.size ? user.displayAvatarURL({ format: 'png', dynamic: true }) : message.author.displayAvatarURL({ format: 'png', dynamic: true })
+        newEmbed.setAuthor('Carrot Bot', 'https://i.ibb.co/v3d9t9x/carrot-clip-art.png')
+            .setThumbnail('https://i.ibb.co/xXQbnn5/user-menu.png')
+            .setTimestamp()
+            .setTitle(`${user ? user.username : message.author.username}'s Avatar`)
+            .setThumbnail(userAvatar)
         message.channel.send(newEmbed)
     }
 }
