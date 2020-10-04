ede7432b33694b7aed32ebc06c1dc18df3529573
Creedon Hood <creedonhood@gmail.com>
2020-10-04 08:15:40 -0600

    Stop Command

---
+++ src/command_modules/stop.ts
@@ -0,0 +1,18 @@
+import { Client, Message } from 'discord.js'
+import { Command } from '../typings'
+
+const stop_client: Command = {
+    name: 'stop',
+    description: 'Stop open client processes.',
+    enabled: true,
+    toggleable: true,
+
+    developerOnly: true,
+
+    execute ({ client, message, args }: { client: Client, message: Message, args: string[] }, Debugging: boolean): string | null | void {
+        console.log(`===STOP COMMAND CALLED, ENDING OPEN PROCESSES===\nCALLER: ${message.author.tag}`)
+        process.exit(0)
+    }
+}
+
+export default stop_client as Command
