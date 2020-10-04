7cc24845afaf8603fea55dd8b4e99d5f82c84a9f
Creedon Hood <creedonhood@gmail.com>
2020-10-04 06:22:43 -0600

    Avatar Image Size

--- src/command_modules/avatar.ts
+++ src/command_modules/avatar.ts
@@ -13,7 +13,7 @@ const avatar: Command = {
         const newEmbed = new MessageEmbed()
         const user = message.mentions.users.map(userObject => userObject).shift()
         console.log(user)
-        const userAvatar = message.mentions.users.size ? user.displayAvatarURL({ format: 'png', dynamic: true }) : message.author.displayAvatarURL({ format: 'png', dynamic: true })
+        const userAvatar = message.mentions.users.size ? user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }) : message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 })
         newEmbed.setAuthor('Carrot Bot', 'https://i.ibb.co/v3d9t9x/carrot-clip-art.png')
             .setThumbnail('https://i.ibb.co/xXQbnn5/user-menu.png')
             .setTimestamp()
