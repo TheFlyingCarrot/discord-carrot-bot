ea73c59b87bf0656619163cfd4974efff46f65de
Creedon Hood <creedonhood@gmail.com>
2020-12-03 19:36:41 -0700

    typings included

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,47 +2,47 @@ import { Client, Message, PermissionString } from 'discord.js'
 import Discord from './internal'
 
 declare module '*.json' {
-    const value: any
-    export default value
+	const value: any
+	export default value
 }
 
 declare interface ExtendedClient extends Discord.Client {
-    commands?: Discord.Collection<any, any>
-    shackled?: boolean
+	commands?: Discord.Collection<any, any>
+	shackled?: boolean
 }
 
 declare interface Command {
-    name: string
-    description: string
-    enabled: boolean
-    toggleable: boolean
+	name: string
+	description: string
+	enabled: boolean
+	toggleable: boolean
 
-    aliases?: string[]
-    usage?: string
-    args?: boolean
-    cooldown?: number
+	aliases?: string[]
+	usage?: string
+	args?: boolean
+	cooldown?: number
 
-    guildOnly?: boolean
-    permission?: PermissionString
-    guildSpecific?: string[]
+	guildOnly?: boolean
+	permission?: PermissionString
+	guildSpecific?: string[]
 
-    developerOnly?: boolean
+	developerOnly?: boolean
 
-    execute?: ({ client, message, args }: { client: Client, message: Message, args: string[] }) => string | null | void | Promise<any>
+	execute?: ({ client, message, args }: { client: Client, message: Message, args: string[] }) => string | null | void | Promise<any>
 }
 
 declare interface ReactionRoleConfig {
-    title: string
-    guild_id: string
-    role_channel_id: string
-    reaction_roles: ReactionRole[]
+	title: string
+	guild_id: string
+	role_channel_id: string
+	reaction_roles: ReactionRole[]
 }
 
 declare interface ReactionRole {
-    name: string
-    category: string
-    role_id: string
-    emoji_name: string
-    emoji_id: string
-    emoji_tag: string
+	name: string
+	category: string
+	role_id: string
+	emoji_name: string
+	emoji_id: string
+	emoji_tag: string
 }
