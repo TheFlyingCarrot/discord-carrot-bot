6eebba3a62e1184d94add1cfea49439ea667c1de
Creedon Hood <creedonhood@gmail.com>
2020-12-09 15:42:17 -0700

    add isValidReaction.ts - refactor from module

---
+++ src/helper_modules/isValidReaction.ts
@@ -0,0 +1,17 @@
+import Discord, { client } from '../internal.js'
+import { fetchPartial } from './fetchPartial'
+
+export async function isValidReaction (messageReaction: Discord.MessageReaction, user: Discord.User): Promise<boolean> {
+	if (messageReaction.partial)
+		await fetchPartial(messageReaction)
+	if (user.partial)
+		await fetchPartial(user)
+
+	if (messageReaction.message.channel.type == 'dm'
+		|| messageReaction.message.channel.name != 'role-request'
+		|| (messageReaction.message
+			&& messageReaction.message.author
+			&& !messageReaction.message.author.equals(client.user)))
+		return
+	return false
+}
