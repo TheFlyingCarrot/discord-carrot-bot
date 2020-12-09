541510e40f2582467fc67700c13b9305d61f5b96
Creedon Hood <creedonhood@gmail.com>
2020-12-09 01:18:52 -0700

    actual guild-info command

--- src/command_modules/guild-info.ts
+++ src/command_modules/guild-info.ts
@@ -1,3 +1,4 @@
+import filesys from 'fs'
 import { Command, ExtendedClient } from '../typings.js'
 import Discord, { Client, Message, MessageEmbed } from '../internal.js'
 
@@ -7,11 +8,20 @@ const guild_info: Command = {
 	enabled: true,
 	toggleable: true,
 
+	aliases: ['guildinfo'],
+
 	guildOnly: true,
+
 	developerOnly: true,
 
 	execute ({ client, message, args }: { client: Client, message: Message, args: string[] }): void {
-		console.log('[Guild Info]', message.guild)
+		try {
+			filesys.writeFileSync(`dump/${message.guild.id}.json`, message.guild.toString())
+			message.reply("Here's the info for this guild.", { files: [`dump/${message.guild.id}.json`] })
+			if (filesys.existsSync(`dump/${message.guild.id}.json`)) filesys.unlinkSync(`dump/${message.guild.id}.json`)
+		} catch (error) {
+			console.error(error)
+		}
 	}
 }
 
