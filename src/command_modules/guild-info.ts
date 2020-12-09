188bdedddda20ce0c968ca35fee6f0f7c0ff5cfa
Creedon Hood <creedonhood@gmail.com>
2020-12-09 01:25:51 -0700

    guildinfo fix

--- src/command_modules/guild-info.ts
+++ src/command_modules/guild-info.ts
@@ -16,9 +16,9 @@ const guild_info: Command = {
 
 	execute ({ client, message, args }: { client: Client, message: Message, args: string[] }): void {
 		try {
-			filesys.writeFileSync(`dump/${message.guild.id}.json`, message.guild.toString())
-			message.reply("Here's the info for this guild.", { files: [`dump/${message.guild.id}.json`] })
-			if (filesys.existsSync(`dump/${message.guild.id}.json`)) filesys.unlinkSync(`dump/${message.guild.id}.json`)
+			filesys.writeFileSync(`${message.guild.id}.json`, message.guild.toString())
+			message.reply("Here's the info for this guild.", { files: [`${message.guild.id}.json`] })
+			if (filesys.existsSync(`${message.guild.id}.json`)) filesys.unlinkSync(`${message.guild.id}.json`)
 		} catch (error) {
 			console.error(error)
 		}
