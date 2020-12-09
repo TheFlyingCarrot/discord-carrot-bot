546194f75318a3ef4c17dfa39036d86945ded9e9
Creedon Hood <creedonhood@gmail.com>
2020-12-08 21:27:13 -0700

    update messageDelete_Handler.ts, footer/msg

--- src/handlers/messageDelete_Handler.ts
+++ src/handlers/messageDelete_Handler.ts
@@ -3,7 +3,7 @@ import Discord, { Client, Collection, Message, MessageEmbed } from '../internal.
 import { ClientUser } from 'discord.js'
 
 export async function handleMessageDeletion (message: Message) {
-	if (message.channel.type === "dm" || message.channel.name === "logs" || !message.guild.available) return
+	if (message.channel.type === "dm" || message.channel.name === "logs" || message.embeds || !message.guild.available) return
 
 	const logChannel = message.guild.channels.cache.find(channel => channel.name === "logs" && channel.type === "text") as Discord.TextChannel
 	if (!logChannel) return
@@ -12,7 +12,7 @@ export async function handleMessageDeletion (message: Message) {
 		const fetchedLogs = await message.guild.fetchAuditLogs({ limit: 1, type: "MESSAGE_DELETE" })
 		const deletionLog = fetchedLogs.entries.first()
 
-		const { author, channel, content, embeds, id } = message
+		const { author, channel, cleanContent, id } = message
 		const { executor, reason } = deletionLog
 
 		const newEmbed = new MessageEmbed()
@@ -23,10 +23,9 @@ export async function handleMessageDeletion (message: Message) {
 			.setTitle('Message Deleted')
 			.addField(`Author`, `${author}`, true)
 			.addField(`Channel`, `${channel}`, true)
-			.addField("Message", content || embeds.toString() || "")
-			.addField("Executor", executor || "Unknown", true)
-			.addField("Reason", reason || "Unspecified", true)
-			.setFooter(`Message ID: ${id} | Author ID: ${author.id} | ${process.env.ENV_TYPE == 'test' ? 'Test Build' : null}`)
+			.addField("Reason", reason || 'Unspecified', true)
+			.addField("Message", cleanContent)
+			.setFooter(`Message ID: ${id} | Author ID: ${author.id} ${process.env.ENV_TYPE == 'test' ? '| Test Build' : ''}`)
 
 		logChannel.send(newEmbed)
 	} catch (error) {
