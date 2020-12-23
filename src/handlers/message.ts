83b477aedba45ab623f7acf561753e1150cdc30f
Creedon Hood <creedonhood@gmail.com>
2020-12-23 07:35:47 -0700

    nullish operator

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -2,8 +2,8 @@ import { client, Config, Discord, HelperModules } from '../internal.js'
 
 export function onMessage (message: Discord.Message): Promise<void> {
 	if (client.events.message === false && !Config.developers.includes(message.author.id.toString())) return
-	// nullish coalescing operator: ??
-	const { command, args } = HelperModules.getCommand({ client, message, prefix: Config.prefix, developers: Config.developers }) || {}
+	// nullish coalescing operator: ?? or, or: ||
+	const { command, args } = HelperModules.getCommand({ client, message, prefix: Config.prefix, developers: Config.developers }) ?? {}
 	if (command && !HelperModules.cooldown({ message, command })) {
 		message.channel.startTyping()
 		try {
