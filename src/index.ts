8e1afdfef316ff7f0ddd2eadaf33e0c10f92a7e9
Creedon Hood <creedonhood@gmail.com>
2020-12-23 07:35:10 -0700

    quick fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { Command, Config, Discord, EventHandlers, ExtendedClient, fs } from './internal'
 
-try {
-	const 
-} catch (error) {
-	
-}
-
 // Client Set-up
 export const client: ExtendedClient = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 client.commands = new Discord.Collection()
