6338816ea81b7b12ebfd0d8069aae5a9e6d3fefd
Creedon Hood <creedonhood@gmail.com>
2020-12-23 09:24:22 -0700

    reformat getCommand.ts, impl. unnecessary returns removed

--- src/helper_modules/getCommand.ts
+++ src/helper_modules/getCommand.ts
@@ -1,59 +1,51 @@
-import { Command, ExtendedClient, Discord } from '../internal.js'
+import { Command, Config, Discord, ExtendedClient } from '../internal.js'
 
-export function getCommand ({ client, message, prefix, developers }: { client: ExtendedClient, message: Discord.Message, prefix: string, developers: string[] }): { command: Command, args: string[] | null } | null {
-	if (!message.content.startsWith(prefix) || message.author.bot || message.tts || message.system) {
-		return null
+export function getCommand ({ client, message }: { client: ExtendedClient, message: Discord.Message }): { command: Command, args: string[] | null } | null {
+	if (!message.content.startsWith(Config.prefix) || message.author.bot || message.tts || message.system) {
+		return
 	}
-	const args = message.content.slice(prefix.length).split(/ +/u)
+	const args = message.content.slice(Config.prefix.length).split(/ +/u)
 	const commandName = args.shift().toLowerCase()
 	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+	if (command === undefined) return
 	try {
-		// Command Presence Check
-		if (!command) {
-			return null
+		// Command(s) enabled?
+		if (!command.enabled || (!client.commandsEnabled && !Config.developers.includes(`${message.author.id}`))) {
+			// message.reply(`\`${command.name}\` is currently \`disabled\`. Sorry!`)
+			return
 		}
-		// Command Toggle Check
-		if (!command.enabled) {
-			message.reply(`\`${command.name}\` is currently \`disabled\`. Sorry!`)
-			return null
-		}
-		// Command Arguments Check
-		if (command.args && !args.length) {
-			message.reply(`That command requires arguments.${command.usage ? `\nThe proper usage is: \`${prefix}${command.name} ${command.usage}\`` : ''}`)
-			return null
-		}
-		// Developer Command Check
-		if (command.developerOnly && !developers.includes(message.author.id.toString())) {
+		// Command is dev-only?
+		if (command.developerOnly && !Config.developers.includes(message.author.id)) {
 			message.reply('That command can only be used by developers. Sorry!')
-			return null
+			return
 		}
-		// Client Shackle Check
-		if (!client.commandsEnabled && !developers.includes(`${message.author.id}`)) {
-			return null
-		}
-		// Guild-Only Command Check
+		// Command is guild-only?
 		if (command.guildOnly && message.channel.type !== 'text') {
 			message.reply('That command is reserved for servers only. Sorry!')
-			return null
+			return
 		}
-		// Guild-Specific Command Check
+		// Command is guild-specific?
 		if (command.guildSpecific && !command.guildSpecific.includes(message.guild.id)) {
 			message.reply('That command is reserved for certain servers only. Sorry!')
-			return null
+			return
 		}
-		// Permissions Check
+		// Command requires permissions?
 		if (command.permission && !message.guild.me.hasPermission(command.permission, { checkAdmin: true })) {
 			message.reply(`I lack the \`${command.permission}\` permission to execute that command.`)
-			return null
+			return
 		} else if (message.member && !message.member.hasPermission(command.permission, { checkAdmin: true, checkOwner: true })) {
 			message.reply('You do not have permission to use that command.')
-			return null
+			return
+		}
+		// Command requires args?
+		if (command.args && !args.length) {
+			message.reply(`That command requires arguments.${command.usage ? `\nThe proper usage is: \`${Config.prefix}${command.name} ${command.usage}\`` : ''}`)
+			return
 		}
 	} catch (error) {
 		console.error(error)
 		message.reply(`\`${command.name}\` caused an internal error and has been cancelled.`)
-		return null
+		return
 	}
-
 	return { command, args }
 }
