5eb22e668324440689e691da5df2d862b1c31db9
Creedon Hood <creedonhood@gmail.com>
2020-12-27 23:51:23 -0700

    type safe

--- src/client_modules/getClientCommand.ts
+++ src/client_modules/getClientCommand.ts
@@ -1,6 +1,6 @@
 import { client, Command } from '../internal'
 
-export function getClientCommand (commandName: string): Command {
+export function getClientCommand (commandName: string): Command | undefined {
 	const command: Command = client.commands.get(commandName) || client.commands.find((command: Command) => command.aliases.includes(commandName))
 	return command
 }
