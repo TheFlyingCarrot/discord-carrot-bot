42a7389a828bd3291804709ec0d1788e97a33c4c
Creedon Hood <creedonhood@gmail.com>
2020-12-29 18:39:42 -0700

    update to guildMemberRemove.ts

--- src/handlers/guildMemberRemove.ts
+++ src/handlers/guildMemberRemove.ts
@@ -1,14 +1,23 @@
-import { client, Discord } from '../internal.js'
+import { client, Config, Discord } from '../internal.js'
 
 export async function onGuildMemberRemove (member: Discord.GuildMember): Promise<void> {
+	const now = Date.now()
+
 	if (client.events.messageDelete === false || !member.guild.available) return
 
 	const eventLog: Discord.GuildAuditLogsEntry = (await member.guild.fetchAuditLogs({ limit: 1, type: 'MEMBER_KICK' })).entries.first()
+	
 	if (!eventLog) {
-		console.log(`User: ${member.user.tag} was banned, but no relevant audit logs were found.`)
+		console.log(`User: ${member.user.tag} was removed, but no relevant audit logs were found.`)
 		return
 	}
-	if (eventLog.action != 'MEMBER_KICK') return
+	
+	if ((eventLog.createdTimestamp - now) >= Config.log_tolerance_ms) {
+		console.log(`User: ${member.user.tag} was removed, but no recent log from the time of the event trigger was found.`)
+		return
+	} else if (eventLog.action !== 'MEMBER_KICK') {
+		return
+	}
 
 	const logChannel = member.guild.channels.cache.find(channel => channel.name === 'logs' && channel.type === 'text') as Discord.TextChannel
 	if (!logChannel) return
