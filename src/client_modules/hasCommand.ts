83384a6e3e60487304bce305b74b6bdc963727fc
Creedon Hood <creedonhood@gmail.com>
2021-01-08 11:54:01 -0700

    dupe type

--- src/client_modules/hasCommand.ts
+++ src/client_modules/hasCommand.ts
@@ -2,7 +2,7 @@ import { client, Command } from '../internal'
 
 export function hasCommand (commandName: string): boolean {
 	if (client.commands.get(commandName)
-		|| client.commands.find((command: Command) => command.aliases ? command.aliases.includes(commandName) : false))
+		|| client.commands.find((command) => command.aliases ? command.aliases.includes(commandName) : false))
 		return true
 	else
 		return false
