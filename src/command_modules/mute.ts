8f0294d600264d552b3b8279ad34c2035815bef9
Creedon Hood <creedonhood@gmail.com>
2021-01-09 13:57:06 -0700

    mute command hotfix

--- src/command_modules/mute.ts
+++ src/command_modules/mute.ts
@@ -1,4 +1,4 @@
-import { Command } from '../internal.js'
+import { Command, Config } from '../internal.js'
 
 const mute: Command = {
 	name: 'mute',
@@ -10,15 +10,23 @@ const mute: Command = {
 		const targetUser = message.mentions.members.first()
 		args.pop()
 
-		targetUser.roles.add('Muted', args.join(' '))
+		if (!Config.muted_role_ids[message.guild.id]) {
+			message.reply('There is no configured muted role for this server.')
+			return
+		}
+		const mutedRoleID = Config.muted_role_ids[message.guild.id].muted_role_ids
+
+		targetUser.roles.add(message.guild.roles.cache.find((role) => role.id === mutedRoleID), args.join(' '))
+			.catch(console.error)
 
 		message.reply(`${targetUser.user.tag} was muted.`)
 			.then((sentMessage) => {
 				setTimeout(() => {
 					if (sentMessage.deletable) sentMessage.delete({ reason: 'Mute command.' })
 					if (message.deletable) message.delete({ reason: 'Mute command.' })
-				}, 2500)
+				}, 1250)
 			})
+			.catch(console.error)
 	}
 }
 
