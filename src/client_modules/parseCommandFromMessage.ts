48c994623afc9aa190b093d2d698584642ecf3b7
Creedon Hood <creedonhood@gmail.com>
2021-03-21 20:19:07 -0600

    return default empty object to return nothing

--- src/client_modules/parseCommandFromMessage.ts
+++ src/client_modules/parseCommandFromMessage.ts
@@ -1,15 +1,17 @@
 import { config, DiscordJS, getCommand } from '../internal'
 import { Command } from '../typings'
 
+const DefaultParsedMessage = { args: [], command: null, message: null }
+
 /**
  * @param message Discord message.
  * @returns Object containing `args`, `command` of type Command, and the original message.
  */
 export function parseCommandFromMessage (message: DiscordJS.Message): { args: string[] | null, command: Command | null, message: DiscordJS.Message } {
-	if (!message.content.startsWith(config.prefix)) return
-	if (message.author.bot) return
-	if (message.system) return
-	if (message.tts) return
+	if (!message.content.startsWith(config.prefix)) return DefaultParsedMessage
+	if (message.author.bot) return DefaultParsedMessage
+	if (message.system) return DefaultParsedMessage
+	if (message.tts) return DefaultParsedMessage
 
 	const input = message.content.slice(config.prefix.length)
 
